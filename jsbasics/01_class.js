4ae31d7ee7a1fc49052acd68a9bf58df39e67c31
dineshkm123 <137782528+dineshkm123@users.noreply.github.com>
2023-10-14 15:58:25 +0000

    basicdatatypes

---
+++ jsbasics/01_class.js
@@ -0,0 +1,10 @@
+const studentName="dinesh"
+let studentId=1234
+studentBranch="ise"
+studentBranch="cse"
+console.log(studentBranch);
+/*
+var in not used in js because var do not support block scope
+
+*/
+console .table([studentId,studentName,studentBranch])
---
+++ jsbasics/02_class.js
@@ -0,0 +1,11 @@
+  "use strict";//treatall js code as newer version
+  // alert(3+3 ) cannot be used bcs we anre runing node js not browser
+console.log(3+3)
+// null is stand alone value// it is the representation for empty value
+
+//undefined---> no value is assigned to the variable 
+//null and undefined are not same undefined is not null
+//symbol to find the uniqueness
+console.log(typeof(2))
+console.log(typeof null)
+//null is object 
